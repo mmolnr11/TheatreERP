6ee71ad6a6282c86dc0fe248d7ccbd280ba2dd5b
Andras Szabo <andras.l.szabo@gmail.com>
2018-03-07 15:25:37 +0100

    Security login with admin

--- src/main/java/com/project/config/UserDetailConfig.java
+++ src/main/java/com/project/config/UserDetailConfig.java
@@ -11,22 +11,39 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.management.relation.Role;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.logging.Logger;
 
 @Service
-public class UserDetailConfig implements UserDetailsService{
+public class UserDetailConfig implements UserDetailsService {
+
     @Autowired
     private UserRepository userRepository;
 
 
     @Override
     @Transactional(readOnly = true)
-    public UserDetails loadUserByUsername(String emailAddress) throws UsernameNotFoundException{
-        User user = userRepository.getUserByEmailAddress(emailAddress);
-        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
-        grantedAuthorities.add(new SimpleGrantedAuthority("USER"));
-        grantedAuthorities.add(new SimpleGrantedAuthority("ADMIN"));
-        return new org.springframework.security.core.userdetails.User(user.getEmailAddress(), user.getPassword(),grantedAuthorities);
+    public UserDetails loadUserByUsername(String emailAddress) throws UsernameNotFoundException {
+       try {
+           User user = userRepository.getUserByEmailAddress(emailAddress);
+           if(user == null){
+               System.out.println("user not found with the provided email");
+               return null;
+           }
+           return new org.springframework.security.core.userdetails.User(user.getEmailAddress(), user.getPassword(), getAuthorities(user));
+       }catch (Exception e){
+           throw new UsernameNotFoundException("Email not found ");
+       }
+    }
+
+
+    private Set<GrantedAuthority> getAuthorities(User user) {
+        Set<GrantedAuthority> authorities = new HashSet<>();
+        GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(user.getRole());
+        authorities.add(grantedAuthority);
+        System.out.println("user authorities are " + authorities.toString());
+        return authorities;
     }
 }
